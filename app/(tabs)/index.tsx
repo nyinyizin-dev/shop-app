56ac2379e19b61d9e942d441dc810bac0d738e79
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-06 18:54:24 +0900

    Inline & External Style

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,42 @@
-import {  Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import POSTS from "@/data/posts.json"
+import POSTS from "@/data/posts.json";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <Text style={{ fontSize: 18,marginBottom: 10 }}>Hello Local Storage</Text>
-      {POSTS.posts.map(post => (
-        <Text key={post.id} style={{marginBottom: 10, fontSize: 14}}>
-        Title : {post.title}
+      <Text style={styles.text}>Hello Local Storage</Text>
+      {POSTS.posts.map((post) => (
+        <Text
+          key={post.id}
+          style={[styles.text, styles.postText, { fontWeight: "bold" }]}
+        >
+          Title :{" "}
+          <Text style={{ fontWeight: "300", fontSize: 14 }}> {post.title}</Text>
+          {"\n"}
+          Body :{" "}
+          <Text style={{ fontWeight: "300", fontSize: 14 }}> {post.body}</Text>
         </Text>
+       
+
       ))}
+      <Text style={{ fontWeight: "bold" }}>
+        This is Parent Text. <Text>This is Nested Text</Text>
+      </Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "blue",
+  },
+  postText: {
+    marginBottom: 10,
+    fontSize: 16,
+    color: "teal",
+  },
+});
