a68c9593e34074e5f6b31e188c802f0019126686
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-06 18:26:33 +0900

    Offline Data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,17 @@
-import React, { useState } from "react";
-import { Button, Text, TextInput } from "react-native";
-import { useMMKVStorage } from "react-native-mmkv-storage";
-
-import { MMKV, MMKVENC } from "@/mmkv/store";
+import {  Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import POSTS from "@/data/posts.json"
+
 export default function HomeScreen() {
-  const [name, setName] = useState("");
-  const [user, setUser] = useMMKVStorage("user", MMKV, "unknown");
-  // const [policy, setPolicy] = useMMKVStorage("policy", SETTING, "");
-  const [token, setToken] = useMMKVStorage("token", MMKVENC, "");
-
-  const setString = async () => {
-    await MMKV.setStringAsync("user", name);
-  };
-
   return (
     <SafeAreaView>
-      <Text style={{ fontSize: 24 }}>Home Screen</Text>
-      <Text style={{ fontSize: 24 }}>Hello - {name}</Text>
-      <TextInput
-        placeholder="Enter your nmae"
-        onChangeText={(val) => setName(val)}
-        defaultValue={name}
-        style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
-      />
-      <Button title="Save to MMKV" onPress={() => setUser(name)} />
-      <Text style={{ fontSize: 24 }}>{user}</Text>
-      <Button title="Save to setString MMKV" onPress={setString} />
-      <Text style={{ fontSize: 24 }}>Token : {token}</Text>
-      <Button title="Save to MMKVENC" onPress={()=>setToken('asdf1234')} />
+      <Text style={{ fontSize: 18,marginBottom: 10 }}>Hello Local Storage</Text>
+      {POSTS.posts.map(post => (
+        <Text key={post.id} style={{marginBottom: 10, fontSize: 14}}>
+        Title : {post.title}
+        </Text>
+      ))}
     </SafeAreaView>
   );
 }
