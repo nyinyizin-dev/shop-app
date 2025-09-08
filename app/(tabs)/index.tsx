ec5577d71fc87388424a67f0cf48da8c328e241c
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-08 16:31:08 +0900

    flexGrow, flexShrink,  flexBasis

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -7,22 +7,29 @@ export default function HomeScreen() {
     <SafeAreaView
       style={{
         flex: 1,
-        flexWrap: "wrap",
         flexDirection: "row",
-        gap: 10,
-        alignContent: 'center',
-        alignItems:'center',
-        justifyContent: 'center',
       }}
     >
-      <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "#CA64ae", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "#f3d31fed", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "#f3d31fed", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "#CA64ae", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
+      <View
+        style={{
+          // flex: 1,
+          flexGrow: 1,
+          // flexShrink: 1,  // default - 0
+          // flexBasis: 0, // default - auto
+        }}
+      >
+        <Image source={require("@/assets/images/mountain.jpg")} />
+      </View>
+      <View
+        style={{
+          // flex: 1,
+          flexGrow: 1,
+          // flexShrink: 1,
+          // flexBasis: 0,
+        }}
+      >
+        <Image source={require("@/assets/images/mountain.jpg")} />
+      </View>
     </SafeAreaView>
   );
 }
