6fc052cf23832ee121ee7a63b60b20f791ab749d
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-08 18:47:33 +0900

    Data fetching with error handling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,13 @@
-import { StyleSheet, Text, View, Image, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-// https://reqres.in/api/users/2
-// https://reqres.in/ - domain
-// api/users - route
-// /2 - route param
-// ?page=2 - query string
-
-// {
-//     "data": {
-//         "id": 2,
-//         "email": "janet.weaver@reqres.in",
-//         "first_name": "Janet",
-//         "last_name": "Weaver",
-//         "avatar": "https://reqres.in/img/faces/2-image.jpg"
-//     },
-//     "support": {
-//         "url": "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
-//         "text": "Tired of writing endless social media content? Let Content Caddy generate it for you."
-//     }
-// }
-
-// fetch()
-// axios
-// tanstack query
-// rtk query
-
-// API Fetching - Data Fetching
-// Request - Response
 
 type User = {
   id: number;
@@ -40,17 +19,29 @@ type User = {
 
 export default function HomeScreen() {
   const [user, setUser] = useState<User | null>();
+  const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = () => {
-    return fetch("https://reqres.in/api/users/2", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((json) => setUser(json.data))
-      .catch((error) => console.log(error));
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://reqres.in/api/users/2", {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        console.log("Data Fetching Fail");
+        return null;
+      }
+
+      const json = await response.json();
+      setUser(json.data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -58,9 +49,10 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <SafeAreaView style={{flex: 1}} >
+    <SafeAreaView style={{ flex: 1 }}>
       <Text style={styles.text}>Hello Networking</Text>
-      <Text >{user?.email}</Text>
+      {isLoading && <ActivityIndicator />}
+      {user ? <Text>{user?.email}</Text> : <Text>No User Found</Text>}
     </SafeAreaView>
   );
 }
