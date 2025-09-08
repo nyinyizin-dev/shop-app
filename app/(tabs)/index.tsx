88c169c114210dfde8ad15e21f3ef9ca28ec2adb
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-08 21:28:27 +0900

    setup ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,40 +1,15 @@
-import { StyleSheet, Text, ActivityIndicator } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-import useQuery from "@/hooks/useQuery";
-
-type Response = {
-  data: {
-    id: number;
-    email: string;
-    first_name: string;
-    last_name: string;
-    avatar: string;
-  };
-};
+import { Image } from "expo-image";
 
 export default function HomeScreen() {
-  const { data, isLoading, error } = useQuery<Response>("users/2");
-
-  if (error) {
-    return <Text>{error}</Text>;
-  }
-
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Text style={styles.text}>Hello Custom Hook</Text>
-      {isLoading && <ActivityIndicator />}
-      {data ? <Text>{data.data.email}</Text> : <Text>No User Found</Text>}
+    <SafeAreaView>
+      <Text>Hello UI</Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    marginTop: 50,
-    color: "#CA64E3",
-    fontSize: 30,
-    textAlign: "center",
-    fontWeight: "bold",
-  },
+  text: {},
 });
