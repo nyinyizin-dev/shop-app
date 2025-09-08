e20715af596d8737220ff1b41404f6ad835a4b31
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-08 15:27:25 +0900

    flex box

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,23 @@
-import { StyleSheet, Text, View,  } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <View style={{backgroundColor: '#CA64E3', width: '50%', height: '50%'}} />
-      <View style={{backgroundColor: '#CA64ae', width: '100%', height: "50%"}} />
+    <SafeAreaView style={{ flex: 1,flexDirection:"row" }}>
+      <View style={{ backgroundColor: "#CA64E3", flex: 1 }} />
+      <View style={{ backgroundColor: "#CA64ae", flex: 2 }} />
+      <View style={{ backgroundColor: "#CA64", flex: 3 }} />
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   text: {
     marginTop: 50,
-    color: 'rgba(243, 211, 31, 0.93)',
+    color: "rgba(243, 211, 31, 0.93)",
     fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  }
-})
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
