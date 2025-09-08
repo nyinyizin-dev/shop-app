a329ba11d4874e8bc67626f578d9209518a8633e
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-08 16:14:17 +0900

    flexWrap

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,22 +7,22 @@ export default function HomeScreen() {
     <SafeAreaView
       style={{
         flex: 1,
-        flexWrap: 'wrap',
+        flexWrap: "wrap",
         flexDirection: "row",
-        gap:10
-        // alignItems:'center',
-        // justifyContent: 'between',
+        gap: 10,
+        alignContent: 'center',
+        alignItems:'center',
+        justifyContent: 'center',
       }}
     >
       <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "#CA64ae", width: 100, height: 100 }} />
-      {/* <View style={{alignSelf:'flex-end', backgroundColor: "#f3d31fed", width: 100, height: 100 }} /> */}
       <View style={{ backgroundColor: "#f3d31fed", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
-       <View style={{ backgroundColor: "#f3d31fed", width: 100, height: 100 }} />
-        <View style={{ backgroundColor: "#CA64ae", width: 100, height: 100 }} />
-           <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "#f3d31fed", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "#CA64ae", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
     </SafeAreaView>
   );
 }
