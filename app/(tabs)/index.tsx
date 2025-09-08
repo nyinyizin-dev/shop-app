d54cf27334b7ccfc4405f313e0c02d4f1ec3c54d
nyinyizin-dev <nyinyizin450@gmail.com>
2025-09-08 15:39:06 +0900

    alignItems, justifyContent

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,20 +4,27 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={{ flex: 1,flexDirection:"row" }}>
-      <View style={{ backgroundColor: "#CA64E3", flex: 1 }} />
-      <View style={{ backgroundColor: "#CA64ae", flex: 2 }} />
-      <View style={{ backgroundColor: "#CA64", flex: 3 }} />
+    <SafeAreaView
+      style={{
+        flex: 1,
+        // flexDirection: "column",
+        alignItems:'center',
+        justifyContent: 'between',
+      }}
+    >
+      <View style={{ backgroundColor: "#CA64E3", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "#CA64ae", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "#f3d31fed", width: 100, height: 100 }} />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  text: {
-    marginTop: 50,
-    color: "rgba(243, 211, 31, 0.93)",
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   text: {
+//     marginTop: 50,
+//     color: "#f3d31fed",
+//     fontSize: 30,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//   },
+// });
